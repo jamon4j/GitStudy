1cf378fc52851eafbe76de7270d6f99d96a249cd
jjzhang <bupt_zjj@163.com>
2016-06-05 21:13:05 +0800

    hello git

---
+++ src/main/java/cn/zjj/GitStudy/gitPart1/App.java
@@ -0,0 +1,13 @@
+package cn.zjj.GitStudy.gitPart1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
